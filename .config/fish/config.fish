4218a71a82d1289dd14476e988235c0ce162c641
Michael Moreno <michaelmoreno.dev@gmail.com>
2022-03-12 09:12:34 -0700

    Added fish abbr: pacman, sriwd, search. pacman - 'sudo pacman' sriwd - 'sudo systemctl restart iwd.service' search - 'sudo updatedb & locate'

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,8 +6,11 @@ end
 alias dotfiles='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 
 # Abbreviations
+abbr pacman 'sudo pacman'
 abbr clip 'xclip -selection clipboard'
 abbr c 'ccat'
+abbr sriwd 'sudo systemctl restart iwd.service'
+abbr search 'sudo updatedb & locate'
 
 set fish_color_normal normal
 set fish_color_command --bold
